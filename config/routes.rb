1ea4b736eca7fa292d1996bf22b243d3b5114c84
Allen <0allen.hsu@gmail.com>
2014-07-01 19:46:21 +1000

    g+ skele

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :doodles
   resources :users
   root to: 'main#index'
+  get '/loggedin' to: 'main#loggedin'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
